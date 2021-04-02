1742627a9c19b3ef28c500298ff07f7202ef8061
Biswajit Sahoo <sahoob193@gmail.com>
2021-04-02 12:42:17 +0530

    first commit

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\biswajit sahoo\courses\django udemy\carzoneproject\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
