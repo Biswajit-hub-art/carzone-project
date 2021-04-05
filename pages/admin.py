3b75f93e11d3cd0d8567d2145bfc12d63b4e6d40
Biswajit Sahoo <sahoob193@gmail.com>
2021-04-05 19:18:08 +0530

    created postgresql, Team model and Media Files

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import Team
+from django.utils.html import format_html
 # Register your models here.
+
+class TeamAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="40"/>'.format(object.photo.url))
+
+    thumbnail.short_description='Photo'
+    fields=('first_name', 'designation', 'created_date')
+    list_display=['id', 'thumbnail', 'first_name', 'designation', 'created_date']
+    list_display_links=['id', 'first_name']
+    search_fields=['first_name', 'designation', 'last_name']
+    list_filter=['designation']
+
+admin.site.register(Team, TeamAdmin)
--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
+from .models import Team
 
 # Create your views here.
 def home(request):
-    return render(request, 'pages/home.html')
+    teams = Team.objects.all()
+    context = {
+        'teams' : teams
+    }
+    return render(request, 'pages/home.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    teams = Team.objects.all()
+    context={
+        'teams' : teams
+    }
+    return render(request, 'pages/about.html', context)
 
 def services(request):
     return render(request, 'pages/services.html')
