f280d1154d0b73181f29c7347f70faa020072bd7
Biswajit Sahoo <sahoob193@gmail.com>
2021-04-11 07:42:40 +0530

    single car page and pagination

--- cars/urls.py
+++ cars/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from cars import views
 urlpatterns=[
     path('', views.cars, name='cars'),
+    path('car_detail/<int:id>', views.car_detail, name='car_detail'),
+    
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
-
+from .models import Car
+from django.core.paginator import Paginator
 # Create your views here.
 def cars(request):
+    cars_list= Car.objects.order_by('-created_date')
+
+    page = request.GET.get('page')
+    paginator=Paginator(cars_list,3)
+    paged_cars=paginator.get_page(page)
+
     context={
         'car_active': "active",
+        'cars_list':paged_cars,
     }
     return render(request, 'cars/cars.html', context)
+
+
+def car_detail(request,id):
+    detail= Car.objects.get(id=id)
+    context={
+        'detail':detail,
+    }
+    return render(request, 'cars/car_detail.html', context)
